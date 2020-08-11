0f8280e373c23e3eb635307cd1760bf02bc4ccef
Mir Imad Ahmed <imad.ahmed@gsquad.io>
2020-08-11 21:38:39 +0500

    employee done

--- company/employee/get.php
+++ company/employee/get.php
@@ -6,10 +6,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $get_declaration = mysqli_query($con, "SELECT * FROM Declaration where employee_id='$id'");
         $get_location_logs = mysqli_query($con, "SELECT * FROM location_logs where employee_id='$id'");
         $get_daily_checks = mysqli_query($con, "SELECT * FROM Daily_check where employee_id='$id'");
+        $get_meetings = mysqli_query($con, "SELECT * FROM Meeting where employee_id = '$id'");
+        $get_employee = mysqli_query($con, "select * from Employee where employee_id='$id'");
         $temp = array();
         $temp['declaration'] = array();
         $temp['location_logs'] = array();
         $temp['daily_checks'] = array();
+        $temp['meetings'] = array();
+        $temp['employee'] = array();
         while ($declaration = mysqli_fetch_array($get_declaration)) {
             array_push($temp['declaration'], $declaration);
         }
@@ -18,6 +22,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         }
         while ($daily_check = mysqli_fetch_array($get_daily_checks)) {
             array_push($temp['daily_checks'], $daily_check);
+        }
+        while ($meeting = mysqli_fetch_array($get_meetings)) {
+            $meeting_id = $meeting['meeting_id'];
+            $meeters = array();
+            $get_meeters = mysqli_query($con, "SELECT * FROM `Meeting_Persons` where meeting_id='$meeting_id'");
+            while ($meeter = mysqli_fetch_array($get_meeters)) {
+                array_push($meeters, $meeter['person_name']);
+            }
+            $meeting['meeters'] = $meeters;
+            array_push($temp['meetings'], $meeting);
+        }
+        while ($employee = mysqli_fetch_array($get_employee)) {
+            array_push($temp['employee'], $employee);
         }
         echo json_encode(utf8ize($temp));
     } else {
