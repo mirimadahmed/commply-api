cf7b6b41d30105f2737f973096f672eb560f2a1f
Mir Imad Ahmed <imad.ahmed@gsquad.io>
2020-08-11 22:31:44 +0500

    get company done

--- company/get.php
+++ company/get.php
@@ -1,18 +1,38 @@
 <?php
 include '../connection.php';
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $get_companies = "";
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
         $get_companies = mysqli_query($con, "select * from Company where company_id='$id'");
+        $get_locations = mysqli_query($con, "SELECT * FROM `location` where compId='$id'");
+        $get_owners = mysqli_query($con, "SELECT * FROM `Owner` where company_id='$id'");
+        $get_visitors = mysqli_query($con, "SELECT * FROM `visitor` where compId='$id'");
+        $temp = array();
+        $temp['locations'] = array();
+        $temp['owners'] = array();
+        $temp['visitors'] = array();
+
+        while ($company_row = mysqli_fetch_array($get_companies)) {
+            $temp['company'] = $company_row;
+        }
+        while ($location = mysqli_fetch_array($get_locations)) {
+            array_push($temp['locations'], $location);
+        }
+        while ($owner = mysqli_fetch_array($get_owners)) {
+            array_push($temp['owners'], $owner);
+        }
+        while ($visitor = mysqli_fetch_array($get_visitors)) {
+            array_push($temp['visitors'], $visitor);
+        }
+        echo json_encode(utf8ize($temp));
     } else {
         $get_companies = mysqli_query($con, "select * from Company");
+        $temp = array();
+        while ($company_row = mysqli_fetch_array($get_companies)) {
+            array_push($temp, $company_row);
+        }
+        echo json_encode(utf8ize($temp));
     }
-    $temp = array();
-    while ($company_row = mysqli_fetch_array($get_companies)) {
-        array_push($temp, $company_row);
-    }
-    echo json_encode($temp);
 } else {
     $temp = array();
     $temp['error'] = 1;
